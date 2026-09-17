746740553962c437defe6c537c721bc8840b9076
hassaank17 <hassaansherwani16@gmail.com>
2026-09-17 11:20:11 +0200

    Rename EMAIL_PASSWORD environment variable

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from google import genai
 # Fetch environment variables
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_PASSWORD = os.getenv("SENDER_PASSWORD")
 RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
 
 
